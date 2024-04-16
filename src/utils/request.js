a4efc9b5eeeb1768c63485268b93eda88a75c77d
jingzhini <2964897720@qq.com>
2024-04-17 02:28:44 +0800

    rebuild

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,31 +5,32 @@ import { getToken } from "@/utils/auth";
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: "http://localhost:9090/web",
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000, // request timeout
+  timeout: 20000, // request timeout
 });
 
 // request interceptor
-service.interceptors.request.use(
-  (config) => {
-    // do something before request is sent
+// service.interceptors.request.use(
+//   (config) => {
+//     // do something before request is sent
 
-    // TODO: 配置全局token：
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers["X-Token"] = getToken();
-    }
-    return config;
-  },
-  (error) => {
-    // do something with request error
-    console.log(error); // for debug
-    return Promise.reject(error);
-  }
-);
+//     // TODO: 配置全局token：
+//     if (store.getters.token) {
+//       // let each request carry token
+//       // ['X-Token'] is a custom headers key
+//       // please modify it according to the actual situation
+//       config.headers["X-Token"] = getToken();
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     // do something with request error
+//     console.log(error); // for debug
+//     return Promise.reject(error);
+//   }
+// );
 
 // response interceptor
 service.interceptors.response.use(
